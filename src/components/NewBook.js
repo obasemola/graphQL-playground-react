790c26b1fc821a1780754c09b06b474e5665997b
obasemola <obasemolaayodele@gmail.com>
2020-10-31 22:25:07 +0200

    finishing the frontend

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { ADD_BOOK, ALL_BOOKS } from '../queries/queries'
+import { ADD_BOOK } from '../queries/queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
